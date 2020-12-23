95daa7e5b1e29a1369a8e4812dd9f95482e374cf
Robert Reed <robert.mc.reed@gmail.com>
2020-12-22 22:40:26 -0800

    onHold cb working with vectors

--- include/EasierButton.h
+++ include/EasierButton.h
@@ -9,19 +9,62 @@
 
 #include "Arduino.h"
 #include <EasyButton.h>
+#include <vector>
 
-// typedef void (*voidCallback) ();
-// typedef void (*voidCallbackStr) (String);
-// typedef String (*stringCallback) ();
-// typedef String (*stringCallbackStr) (String);
+typedef void (*voidCallback) ();
+typedef void (*voidCallbackLong) (unsigned long);
+
+typedef std::function<void()> callback;
+typedef std::vector<callback> callback_vector;
+
+typedef std::vector<unsigned long> long_vector;
+typedef std::vector<bool> bool_vector;
 
 class EasierButton
 {
   public:
-    EasierButton();
+    EasyButton easyButton;
+    EasierButton(uint8_t pin);
+    EasierButton(uint8_t pin, bool pullUp);
+    EasierButton(uint8_t pin, uint32_t debounce);
+    EasierButton(uint8_t pin, uint32_t debounce, bool pullUp);
+    EasierButton(uint8_t pin, uint32_t debounce, bool pullUp, bool active_low);
+
+    void begin();
+    void update();
+
+    bool heldAtBoot();
+    bool pressedAtBoot();
+    bool blockBoot(int duration);
+
+    void setOnPressed(voidCallback cb);
+    void setOnPressed(voidCallbackLong cb);
+    void setOnReleased(voidCallback cb);
+    void setOnReleased(voidCallbackLong cb);
+    void setOnHold(unsigned long duration, callback cb);
 
   private:
-    void _flagReboot();
+    bool _lastState;
+    bool _pressedAtBoot;
+    bool _heldAtBoot;
+
+    void _setup();
+    void _handlePressed();
+    void _handleReleased();
+    void _checkAtBoot();
+
+    unsigned long _lastPress;
+    unsigned long _lastRelease;
+
+    voidCallback _onPressed;
+    voidCallbackLong _onPressedLong;
+
+    voidCallback _onReleased;
+    voidCallbackLong _onReleasedLong;
+
+    bool_vector onHoldBools;
+    long_vector onHoldDurations;
+    callback_vector onHoldCallbacks;
 };
 
 #endif
--- src/EasierButton.cpp
+++ src/EasierButton.cpp
@@ -5,8 +5,221 @@
 */
 
 #include "Arduino.h"
+#include <EasyButton.h>
 #include "EasierButton.h"
+#include <vector>
 
-void EasierButton::loop() {
-
+void debug(int val) {
+  #ifdef EASIER_DEBUG
+    Serial.print(val);
+  #endif
 }
+
+void debugln(int val) {
+  #ifdef EASIER_DEBUG
+    Serial.println(val);
+  #endif
+}
+
+void debug(String val) {
+  #ifdef EASIER_DEBUG
+    Serial.print(val);
+  #endif
+}
+
+void debugln(String val) {
+  #ifdef EASIER_DEBUG
+    Serial.println(val);
+  #endif
+}
+
+// constructor
+EasierButton::EasierButton(uint8_t pin, uint32_t debounce, bool pullUp, bool active_low): easyButton(pin, debounce, pullUp, active_low)
+{
+  _setup();
+}
+
+EasierButton::EasierButton(uint8_t pin, uint32_t debounce, bool pullUp): easyButton(pin, debounce, pullUp, pullUp)
+{
+  _setup();
+}
+
+EasierButton::EasierButton(uint8_t pin, uint32_t debounce): easyButton(pin, debounce, true, true)
+{
+  _setup();
+}
+
+EasierButton::EasierButton(uint8_t pin, bool pullUp): easyButton(pin, 35, pullUp, pullUp)
+{
+  _setup();
+}
+
+EasierButton::EasierButton(uint8_t pin): easyButton(pin, 35, true, true)
+{
+  _setup();
+}
+
+// Private Members
+void EasierButton::_setup()
+{
+  _lastState = false;
+  _heldAtBoot = false;
+  _pressedAtBoot = false;
+
+  unsigned long now = millis();
+  _lastPress = now;
+  _lastRelease = now;
+}
+
+void EasierButton::_handlePressed()
+{
+  if (_lastState) return;
+  
+  _lastState = true;
+  unsigned long now = millis();
+  unsigned long pressToPress = now - _lastPress;
+  unsigned long releaseToPress = now - _lastRelease;
+  _lastPress = now;
+
+  debugln(F("Button was pressed."));
+  debug(F("It had been "));
+  debug(pressToPress);
+  debug(F(" ms since the button was last pressed, and "));
+  debug(releaseToPress);
+  debugln(F(" ms since the button was last released."));
+
+  if (_onPressedLong)
+  {
+    _onPressedLong(pressToPress);
+  }
+  else if (_onPressed)
+  {
+    _onPressed();
+  }
+}
+
+void EasierButton::_handleReleased()
+{
+  if (!_lastState) return;
+
+  _lastState = false;
+  unsigned long now = millis();
+  unsigned long pressDuration = now - _lastPress;
+  _lastRelease = now;
+
+  debugln(F("Button was released."));
+  debug(F("The button was pressed for "));
+  debug(pressDuration);
+  debugln(F(" ms."));
+
+  if (_onReleasedLong)
+  {
+    _onReleasedLong(pressDuration);
+  }
+  else if (_onReleased)
+  {
+    _onReleased();
+  }
+}
+
+void EasierButton::_checkAtBoot()
+{
+  if (easyButton.isPressed())
+  {
+    debugln("Button PRESSED at boot.");
+    _pressedAtBoot = true;
+  }
+  else
+  {
+    debugln("Button not pressed at boot.");
+  }
+}
+
+// Public Members
+
+void EasierButton::begin() {
+  easyButton.begin();
+  _checkAtBoot();
+}
+
+bool EasierButton::blockBoot(int duration)
+{
+  bool held = false;
+  if (easyButton.isPressed())
+  {
+    _pressedAtBoot = true;
+    delay(duration);
+    easyButton.read();
+    held = !easyButton.wasReleased();
+  }
+
+  _heldAtBoot = held;
+
+  return held;
+}
+
+void EasierButton::update()
+{
+  easyButton.read();
+
+  for(unsigned int i = 0; i < onHoldCallbacks.size(); i++) {
+    bool called = onHoldBools[i];
+
+    if (called) continue;
+
+    uint32_t timeout = onHoldDurations[i];
+    if (_lastState && easyButton.pressedFor(timeout))
+    {
+      onHoldBools[i] = true;
+      callback  cb = onHoldCallbacks[i];
+      cb();
+    }
+  }
+
+  if (easyButton.wasReleased())
+  {
+    for(unsigned int i = 0; i < onHoldCallbacks.size(); i++)
+    {
+      onHoldBools[i] = false;
+    }
+    
+    _handleReleased();
+  }
+
+  if (easyButton.wasPressed())
+  {
+    _handlePressed();
+  }
+}
+
+bool EasierButton::pressedAtBoot() {
+  return _pressedAtBoot;
+}
+
+bool EasierButton::heldAtBoot() {
+  return _heldAtBoot;
+}
+
+void EasierButton::setOnPressed(voidCallback cb) {
+  _onPressed = cb;
+}
+
+void EasierButton::setOnPressed(voidCallbackLong cb) {
+  _onPressedLong = cb;
+}
+
+void EasierButton::setOnReleased(voidCallback cb) {
+  _onReleased = cb;
+}
+
+void EasierButton::setOnReleased(voidCallbackLong cb) {
+  _onReleasedLong = cb;
+}
+
+void EasierButton::setOnHold(unsigned long duration, callback cb) {
+  onHoldCallbacks.push_back(cb);
+  onHoldDurations.push_back(duration);
+  onHoldBools.push_back(false);
+}
+
+
